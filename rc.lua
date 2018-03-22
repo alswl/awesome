69b4fa9b2c86564cc6d2292eb1e2e5e84684a8a1
alswl <alswlx@gmail.com>
2018-03-22 16:15:31 +0800

    fix: missing menu

--- rc.lua
+++ rc.lua
@@ -1,11 +1,12 @@
 require("lib/error")
 
+
 require("variables")
 require("lib/helper")
+require("menu")
 require("lib/wibox")
 require("lib/mouse")
 require("lib/key")
 require("lib/signals")
 
-require("menu")
 require("rule")
